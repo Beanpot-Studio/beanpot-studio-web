62482c723822d8689033a5d6b6d35f891f215243
Jen Looper <jen.looper@gmail.com>
2025-03-28 21:54:32 -0500

    launch

---
+++ tailwind.config.cjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{astro,html,js,jsx,ts,tsx}', // Include all relevant files
+  ],
+  theme: {
+    extend: {
+      colors: {
+        burgundy: '#631d35',
+        tan: '#874545',
+        black: '#1e211e',
+        taupe: '#cf9465',
+        sienna: '#f4bb75',
+      },
+      fontFamily: {
+        serif: ['DM Serif Display', 'serif'], // For headings
+        sans: ['DM Sans', 'sans-serif'], // For body text
+      },
+    },
+  },
+  plugins: [],
+};
