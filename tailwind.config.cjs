5917b7912519d6830ec84e64a0c03a018ed712ee
Jen Looper <jen.looper@gmail.com>
2025-03-29 23:20:04 -0400

    color fix

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,6 +11,8 @@ module.exports = {
         black: '#1e211e',
         taupe: '#cf9465',
         sienna: '#f4bb75',
+        brown: '#573921',
+        cream: '#f7f2e7',
       },
       fontFamily: {
         serif: ['DM Serif Display', 'serif'], // For headings
